6642c3d4198a3da064b396be9c52e523cb757830
Nicolas Faessel <nicolas.faessel@ynov.com>
2025-06-03 14:06:22 +0200

    remove: default login

--- frontend/src/components/pages/Home.vue
+++ frontend/src/components/pages/Home.vue
@@ -6,25 +6,6 @@
  */
 
 // import HomeTemplate from '@/components/templates/HomeTemplate.vue'
-
-import { onMounted, ref } from 'vue'
-import api from '@/services/api'
-import { login, testCompleteAuth } from '@/services/auth'
-
-const email = ref('')
-const password = ref('')
-async function handleSubmit () {
-  try {
-    const response = await login({ email: email.value, password: password.value })
-    console.log(response.data)
-  }
-  catch (error) {
-    console.error(error)
-  }
-}
-onMounted(() => {
-  testCompleteAuth()
-})
 </script>
 
 <template>
@@ -36,25 +17,5 @@ onMounted(() => {
     <p>
       <VIcon name="ri-flag-line" />  <!-- Exemple icône oh-vue-icon -->
     </p>
-
-    <form @submit.prevent="handleSubmit">
-      <label for="email">Email:</label>
-      <input
-        id="email"
-        v-model="email"
-        type="email"
-        required
-      >
-      <label for="password">Password:</label>
-      <input
-        id="password"
-        v-model="password"
-        type="password"
-        required
-      >
-      <button type="submit">
-        Login
-      </button>
-    </form>
   </HomeTemplate>
 </template>
